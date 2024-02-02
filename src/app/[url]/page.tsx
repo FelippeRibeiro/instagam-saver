2570355564552237fe3b757337f0e48fce8a4b87
FelipeRibeiro <felipper43@gmail.com>
2024-02-02 16:28:11 -0300

    feat: add url search param and remove dinamic route

--- src/app/[url]/page.tsx
+++
@@ -1,7 +0,0 @@
-import Home from "../page";
-
-import React from "react";
-
-export default function page({ params: { url } }: { params: { url: string } }) {
-  return <Home URL={url}></Home>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,38 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-import { toast } from "react-hot-toast";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useSearchParams } from 'next/navigation';
 
-export default function Home({ URL = "" }: { URL?: string }) {
-  const [url, setUrl] = useState(URL);
+export default function Home() {
+  const searchParams = useSearchParams();
+  const [url, setUrl] = useState(searchParams.get('url') || '');
+
+  useEffect(() => {
+    const urlParam = searchParams.get('url');
+    if (urlParam) {
+      setUrl((prv) => urlParam);
+      handleDownload();
+    }
+  }, []);
+
   const [results, setResult] = useState<{ link: string; thumb: string }[]>();
   const [isLoading, setIsLoading] = useState(false);
   async function handleDownload() {
     if (!url.length) {
-      toast.error("Insira um URL!");
+      toast.error('Insira um URL!');
       return;
     }
     setIsLoading(true);
     setResult([]);
-    const query = await fetch("/api", { body: JSON.stringify({ link: url }), method: "POST" });
+    const query = await fetch('/api', { body: JSON.stringify({ link: url }), method: 'POST' });
     if (query.ok) {
       const result = await query.json();
       setResult(result);
-      setUrl("");
+      setUrl('');
     } else {
-      toast.error("Erro ao buscar mídias.");
+      toast.error('Erro ao buscar mídias.');
     }
     setIsLoading(false);
   }
@@ -30,7 +41,7 @@ export default function Home({ URL = "" }: { URL?: string }) {
     <>
       <div className="w-screen h-screen flex flex-col items-center ">
         <div className="w-full p-1 mb-4 flex items-center justify-center shadow-lg">
-          <Image src={"/logo.png"} width={70} height={70} alt="logo"></Image>
+          <Image src={'/logo.png'} width={70} height={70} alt="logo"></Image>
           <h1 className="font-mono text-xl">IG Saver</h1>
         </div>
         <div className="w-[50%] flex flex-col items-center max-lg:w-[90%]">
@@ -41,17 +52,14 @@ export default function Home({ URL = "" }: { URL?: string }) {
             value={url}
             onChange={(e) => setUrl(e.target.value)}
             onKeyDown={(e) => {
-              if (e.key === "Enter") handleDownload();
+              if (e.key === 'Enter') handleDownload();
             }}
           />
-          <button
-            className="border bg-blue-400 px-5 py-2 rounded-md hover:bg-blue-500 m-2"
-            onClick={() => handleDownload()}
-          >
+          <button className="border bg-blue-400 px-5 py-2 rounded-md hover:bg-blue-500 m-2" onClick={() => handleDownload()}>
             Download
           </button>
         </div>
-        {isLoading && <Image src={"/imgs/loader.gif"} width={50} height={50} alt="Loader"></Image>}
+        {isLoading && <Image src={'/imgs/loader.gif'} width={50} height={50} alt="Loader"></Image>}
         {results && (
           <div className="grid grid-cols-5 mt-14 max-sm:grid-cols-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
             {results.map((item) => {
@@ -61,14 +69,7 @@ export default function Home({ URL = "" }: { URL?: string }) {
                   key={item.link}
                 >
                   <Link href={item.link} target="_blank" className="flex flex-col items-center ">
-                    <Image
-                      src={item.thumb}
-                      width={200}
-                      height={300}
-                      alt="item"
-                      placeholder="blur"
-                      blurDataURL="/logo.png"
-                    ></Image>
+                    <Image src={item.thumb} width={200} height={300} alt="item" placeholder="blur" blurDataURL="/logo.png"></Image>
                     <p className="">Download ⬇️</p>
                   </Link>
                 </div>
