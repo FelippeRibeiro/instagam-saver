e1feeb2b62c2c881d78324043f12b62248e137dd
Felippe <felipper43@gmail.com>
2024-08-20 09:33:57 -0300

    WIP: fix link selector

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import cheerio from "cheerio";
+import { NextResponse } from 'next/server';
+import cheerio from 'cheerio';
 
 export async function POST(request: Request) {
   const linksAndThumbs: { link: string; thumb: string }[] = [];
   const { link } = await request.json();
 
   const form = new FormData();
-  form.append("q", link);
-  form.append("t", "midia");
-  form.append("lang", "en");
+  form.append('q', link);
+  form.append('t', 'midia');
+  form.append('lang', 'en');
 
   try {
-    const query = await fetch("https://v3.saveig.app/api/ajaxSearch", {
-      method: "POST",
+    const query = await fetch('https://v3.saveig.app/api/ajaxSearch', {
+      method: 'POST',
       body: form,
       headers: {},
     });
@@ -29,18 +29,20 @@ export async function POST(request: Request) {
     //   }
     // });
 
-    $(".download-items").each((i, el) => {
+    $('.download-items').each((i, el) => {
+      console.log(i);
+
       const div = cheerio.load(el);
-      const thumbEle = div(".download-items__thumb > img").attr();
-      const link = div(".download-items__btn > a").attr()?.href;
+      const thumbEle = div('.download-items__thumb > img').attr();
+      const link = div('.download-items__btn:nth-child(3) > a').attr()?.href;
       if (link)
         linksAndThumbs.push({
-          thumb: thumbEle ? thumbEle["data-src"] || thumbEle["src"] : "",
+          thumb: thumbEle ? thumbEle['data-src'] || thumbEle['src'] : '',
           link,
         });
     });
   } catch (error) {
-    return NextResponse.json({ message: "Not Found" }, { status: 404 });
+    return NextResponse.json({ message: 'Not Found' }, { status: 404 });
   }
   return NextResponse.json(linksAndThumbs);
 }
