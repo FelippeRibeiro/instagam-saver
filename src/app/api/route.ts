893fb46d67c1032f903b294d384b0c9f53f3da7f
Felippe <felipper43@gmail.com>
2024-08-23 10:34:35 -0300

    WIP: temporary fix to stories download

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 
 export async function POST(request: Request) {
   const linksAndThumbs: { link: string; thumb: string }[] = [];
@@ -34,23 +34,73 @@ export async function POST(request: Request) {
     });
     let response = await query.json();
 
-    const obfuscatedCode = response.data;
-    eval(obfuscatedCode);
+    if (response.v === 'v2') {
+      const obfuscatedCode = response.data;
+      eval(obfuscatedCode);
 
-    const $ = cheerio.load(value.innerHTML);
+      const $ = cheerio.load(value.innerHTML);
 
-    $('.download-items').each((i, el) => {
-      const div = cheerio.load(el);
-      const thumbEle = div('.download-items__thumb > img').attr();
-      const link = div('.download-items__btn:nth-child(3) > a').attr()?.href;
-      if (link)
-        linksAndThumbs.push({
-          thumb: thumbEle ? thumbEle['data-src'] || thumbEle['src'] : '',
-          link,
-        });
-    });
+      $('.download-items').each((i, el) => {
+        const div = cheerio.load(el);
+        const thumbEle = div('.download-items__thumb > img').attr();
+        const link = div('.download-items__btn:nth-child(3) > a').attr()?.href;
+        if (link)
+          linksAndThumbs.push({
+            thumb: thumbEle ? thumbEle['data-src'] || thumbEle['src'] : '',
+            link,
+          });
+      });
+    }
+    if (response.v == 'v1') {
+      linksAndThumbs.push(...(await extractLinks(response.data)));
+    }
   } catch (error) {
     return NextResponse.json({ message: 'Not Found' }, { status: 404 });
   }
   return NextResponse.json(linksAndThumbs);
+}
+function getMaxResolutionDownloadLink($: cheerio.CheerioAPI, selectElement: cheerio.Element): string {
+  const options = $(selectElement).find('option');
+  let maxResolutionLink = '';
+  let maxResolution = 0;
+
+  options.each((_, option) => {
+    const value = $(option).attr('value');
+    const resolutionText = $(option).text();
+    const resolutionMatch = resolutionText.match(/(\d+)x(\d+)/);
+
+    if (resolutionMatch) {
+      const resolution = parseInt(resolutionMatch[1], 10) * parseInt(resolutionMatch[2], 10);
+
+      if (resolution > maxResolution) {
+        maxResolution = resolution;
+        maxResolutionLink = value!;
+      }
+    }
+  });
+
+  return maxResolutionLink;
+}
+
+// Função principal para extrair as informações
+async function extractLinks(html: string) {
+  const $ = cheerio.load(html);
+  const downloadItems = $('.download-items');
+
+  const results: { thumb: string; link: string }[] = [];
+
+  downloadItems.each((_, element) => {
+    const thumbLink = $(element).find('.download-items__thumb img').attr('src');
+    const selectElement = $(element).find('.photo-option select')[0];
+    const downloadLink = getMaxResolutionDownloadLink($, selectElement);
+
+    if (thumbLink && downloadLink) {
+      results.push({
+        thumb: thumbLink,
+        link: downloadLink,
+      });
+    }
+  });
+
+  return results;
 }
